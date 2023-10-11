782634f8e82103e44212b5691d7e7847d9c20f44
mydreamis-18 <hitoryok@naver.com>
2023-10-12 00:54:45 +0900

    [feat] 백엔드 폴더 구조 설계

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+// 백 서버 시작
 app.listen(PORT, () => {
   console.log(`<<< PORT : ${PORT} >>>`);
 });
 
 const { sequelize } = require("./model");
-
-// ㅜ 라우터로 빠질 모델들
-const { Cafe } = require("./model");
 
 sequelize.sync({ force: true })
   .then(() => {
@@ -18,11 +16,6 @@ sequelize.sync({ force: true })
     console.log(`<<< DB ERROR >>>\n${err}`);
   })
 
-// app.get("/path", (req, res) => {
-//   // API 함수
-// })
-
-// app.post("/path/:params", (req, res) => {
-//   // API 함수
-//   // DB 로우 데이터 생성, 수정, 삭제, 조회
-// })
+// 라우터 사용 => 모든 API 함수는 라우터 폴더 내에 구현
+const router = require("./router");
+Object.values(router).forEach((rt) => app.use(rt));
---
+++ backend/controller/default_controller.js
@@ -0,0 +1,24 @@
+// ㅜ 이 파일에서 사용할 테이블 불러오기
+const { Cafe } = require("../model");
+// ...
+
+// 컨트롤러 코드 예시
+// request 요청을 받아서 백단 작업 후 response 응답
+module.exports.defaultFn = async (req, res) => {
+    //
+    // const { } = req.params;
+
+    // 데이터 통신이 필요한 경우 무조건 예외 처리 적용하기
+    try {
+        //
+        // DB 작업
+        // ...
+        const defaultData = "defaultData";
+
+        res.send({ success: true, data: defaultData, message: "무엇무엇에 성공하였습니다." });
+        //
+    } catch (err) {
+        console.log(err);
+        res.send({ success: false, message: "무엇무엇에 실패하였습니다." });
+    }
+}
