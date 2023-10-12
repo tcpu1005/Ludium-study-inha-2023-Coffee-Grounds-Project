1a8b8541e8af661a1ce3b25b1cf02c9ebfe79fff
mydreamis-18 <hitoryok@naver.com>
2023-10-12 19:09:41 +0900

    [feat] 프론트엔드 리덕스 폴더 생성, 리덕스 기능은 추후 구현 예정

---
+++ frontend/src/redux/middleware/axios_test.js
@@ -0,0 +1,10 @@
+// 리덕스 구현 전까지 axios 함수 구현은 여기서
+
+
+import axios from "axios";
+
+
+require("dotenv").config();
+const BACK_SERVER_PATH = process.env.BACK_SERVER_PATH || "http://localhost:8080";
+
+
