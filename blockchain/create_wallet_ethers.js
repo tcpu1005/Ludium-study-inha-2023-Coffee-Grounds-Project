87f8da833cbeb5bfe070b741d4effd69f554f69c
mydreamis-18 <hitoryok@naver.com>
2023-10-19 18:37:10 +0900

    [feat] 블록체인 뭄바이 네트워크 잔액 조회 성공

--- blockchain/create_wallet_ethers.js
+++ blockchain/create_wallet_ethers.js
@@ -1,9 +1,10 @@
 const { ethers } = require("ethers");
+require("dotenv").config();
 
 
 // 지갑 생성 성공
-const wallet = ethers.Wallet.createRandom();
-console.log(wallet);
+// const wallet = ethers.Wallet.createRandom();
+// console.log(wallet);
 // HDNodeWallet {
 //     provider: null,
 //     address: '0x7c564eBD81307509daa2Ea46A64b1F179fe6B1Bc',
@@ -22,7 +23,8 @@ console.log(wallet);
 //     depth: 5
 //   }
 
-console.log(Object.keys(wallet));
+
+// console.log(Object.keys(wallet));
 // [
 //     'provider',
 //     'address',
@@ -39,9 +41,51 @@ console.log(Object.keys(wallet));
 
 // 메타마스크에서 개인키로 계정 가져오기 가능
 // 0x12a3fd1fb9c45b3aef6b37e4d1056d691aeb374b7631487bbc912de68bcec3ce
-console.log(wallet.privateKey);
+// console.log(wallet.privateKey);
 
 
 // 메타마스크 주소랑 동일
-console.log(wallet.address);
+// console.log(wallet.address);
 
+
+// provider 가져오는 방법1
+(async () => {
+    //
+    // ㅜ 폴리곤 공식 문서의 RPC 엔드포인트 사용 시 에러 발생
+    // https://wiki.polygon.technology/docs/pos/reference/rpc-endpoints/#rpc-api-methods
+    // JsonRpcProvider가 네트워크를 감지하지 못해 시작할 수 없습니다. 1초 후에 다시 시도하세요(URL이 잘못되었거나 노드가 시작되지 않았을 수 있음)
+    // JsonRpcProvider failed to detect network and cannot start up; retry in 1s (perhaps the URL is wrong or the node is not started) 
+    // const provider = new ethers.JsonRpcProvider("https://rpc-mumbai.matic.today");
+
+
+    // JsonRpcProvider {}
+    const provider = new ethers.JsonRpcProvider("https://rpc.ankr.com/polygon_mumbai");
+    console.log(provider);
+
+
+    const private_key = process.env.JIWON_PRIVATE_KEY;
+    const wallet = new ethers.Wallet(private_key, provider);
+    console.log(wallet);
+
+
+    const wallet_balance = await provider.getBalance(wallet.address);
+    console.log(wallet_balance);
+})();
+
+
+// provider 가져오는 방법2
+(async () => {
+    //
+    // EtherscanProvider { network: Network {}, apiKey: null }
+    const provider = new ethers.EtherscanProvider("maticmum");
+    console.log(provider);
+
+
+    const private_key = process.env.JIWON_PRIVATE_KEY || ethers.Wallet.createRandom(provider);
+    const wallet = new ethers.Wallet(private_key, provider);
+    console.log(wallet);
+
+
+    const wallet_balance = await provider.getBalance(wallet.address);
+    console.log(wallet_balance);
+})();
