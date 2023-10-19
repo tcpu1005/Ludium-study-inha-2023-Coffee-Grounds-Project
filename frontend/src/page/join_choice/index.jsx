5b765b19183174e46ce6a82664a12f823915ccfa
jinnybusock <jinnybusock@gmail.com>
2023-10-20 03:43:45 +0900

    1,2,3,4,

---
+++ frontend/src/page/join_choice/index.jsx
@@ -0,0 +1,36 @@
+const Join_choice_page= ()=> {
+    return (
+
+<div class="e29_180">
+  <div class="e29_182">
+    <div class="e29_183"></div>
+    <div class="e29_184">
+      <div class="e29_185"></div><span  class="e29_186">Welcome</span>
+    </div>
+    <div class="e29_187"></div>
+    <div class="e29_188"></div>
+    <div class="e29_189"></div>
+  </div>
+  <div class="e29_193">
+    <div class="e29_190"></div>
+    <div class="e29_194"></div>
+    <div class="e29_195"></div>
+    <div class="e29_196"></div>
+    <div class="e29_197"></div>
+    <div class="e29_191"></div>
+  </div>
+  <div class="e29_207"></div><span  class="e44_85">회원가입</span>
+  <div class="e44_86"><span  class="e44_87">회원가입</span>
+    <div class="e44_88"></div>
+  </div>
+  <div class="e44_89">
+    <div class="e44_90"></div><span  class="e44_91">카페</span>
+  </div>
+  <div class="e44_93">
+    <div class="e44_94"></div><span  class="e44_95">수거업체</span>
+  </div>
+</div>
+    );
+};
+
+export default Join_choice_page;
