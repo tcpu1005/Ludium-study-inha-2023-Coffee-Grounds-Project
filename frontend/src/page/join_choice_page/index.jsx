4052ab7fcf517ddefcd4c6808890954824f2d57c
mydreamis-18 <hitoryok@naver.com>
2023-10-20 21:02:57 +0900

    [feat] 프론트 2_JOIN_CHOICE 기능 구현

--- frontend/src/page/join_choice_page/index.jsx
+++ frontend/src/page/join_choice_page/index.jsx
@@ -1,6 +1,25 @@
+// 2_JOIN_CHOICE
+
+
+import Big_title_component from "../../component/big_title_component";
+import { Button_2 } from "../../base_style";
+import { useNavigate } from "react-router";
+
+
 const Join_choice_page = () => {
+    //
+
+    const nav = useNavigate();
     return (
-        <div>Join_choice_page</div>
+        <>
+            <Big_title_component title="회원가입" />
+            <div>
+                <Button_2 onClick={() => nav("/join/cafe")}>카페</Button_2>
+            </div>
+            <div>
+                <Button_2 onClick={() => nav("/join/collector")}>수거업체</Button_2>
+            </div>
+        </>
     )
 }
 
