08d178a60919c5230e551f1abf167b1da841303d
mydreamis-18 <hitoryok@naver.com>
2023-10-20 20:42:04 +0900

    [feat] 프론트 5_LOGIN 기능 구현

--- frontend/src/page/login_page/index.jsx
+++ frontend/src/page/login_page/index.jsx
@@ -1,6 +1,59 @@
+// 5_LOGIN
+
+
+import Big_title_component from "../../component/big_title_component";
+import { Button_3 } from "../../base_style";
+import { useRef } from "react";
+import {
+  Input_container,
+  Input_el,
+} from "./style";
+
+
 const Login_page = () => {
+  //
+
+  const login_id_ref = useRef();
+  const login_password_ref = useRef();
+
+
+  const login_button_fn = () => {
+    //
+
+    const id = login_id_ref.current.value;
+    const password = login_password_ref.current.value;
+    console.log(id, password);
+
+
+    // 빈 값에 대한 예외 처리
+    if (!id || !password) {
+      alert("값을 입력해주세요.");
+      return;
+    }
+
+    // 여기서 디스패치 날리면 됩니다.
+  };
+
+
   return (
-    <div>Login_page</div>
+    <>
+      <Big_title_component title="로그인" />
+
+
+      <Input_container>
+        <label htmlFor="login_id" >아이디</label>
+        <Input_el id="login_id" ref={login_id_ref} />
+      </Input_container>
+
+
+      <Input_container>
+        <label htmlFor="login_password">비밀번호</label>
+        <Input_el id="login_password" ref={login_password_ref} type="password" />
+      </Input_container>
+
+
+      <Button_3 onClick={login_button_fn}>로그인</Button_3>
+    </>
   )
 }
 
---
+++ frontend/src/page/login_page/style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+
+export const Input_container = styled.div`
+display: flex;
+justify-content: flex-end;
+`;
+
+
+export const Input_el = styled.input`
+width: 60%;
+`;
