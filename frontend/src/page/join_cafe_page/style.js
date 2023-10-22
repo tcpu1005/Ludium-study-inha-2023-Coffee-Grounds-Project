ac263df389c4ce9165bc2d04fabfcd47f574ee37
KJM0424 <woals424@naver.com>
2023-10-22 12:20:29 +0900

    page3

--- frontend/src/page/join_cafe_page/style.js
+++ frontend/src/page/join_cafe_page/style.js
@@ -1,17 +1,30 @@
 import styled from "styled-components";
 
 
-export const Input_container = styled.div`
-display: flex;
-justify-content: flex-end;
+export const Flex_body_container = styled.div`
+ display : flex;
+ justify-content : space-between; 
+ justify-content: center;
 `;
 
+export const Input_container = styled.div`
+margin-top: 10%;
+margin-bottom : 10%;
+margin-top: 35%;
+
+`;
+
+export const Input_container2 = styled.div`
+margin-top: 10%;
+margin-bottom : 10%;
+`;
 
 export const Input_el = styled.input`
-width: 60%;
+width:  70%;
+height : 4vh;
 `;
 
-
 export const Select_el = styled.select`
-height: 100%;
+height: 50%;
+width: 25%;
 `;
--- frontend/src/page/join_choice_page/index.jsx
+++ frontend/src/page/join_choice_page/index.jsx
@@ -2,8 +2,8 @@
 
 
 import Big_title_component from "../../component/big_title_component";
-import { Button_2 } from "../../base_style";
 import { useNavigate } from "react-router";
+import {Button_choice} from "./style";
 
 
 const Join_choice_page = () => {
@@ -13,11 +13,12 @@ const Join_choice_page = () => {
     return (
         <>
             <Big_title_component title="회원 유형" />
+            
             <div>
-                <Button_2 onClick={() => nav("/join/cafe")}>카페</Button_2>
+                <Button_choice onClick={() => nav("/join/cafe")}>카페</Button_choice>
             </div>
             <div>
-                <Button_2 onClick={() => nav("/join/collector")}>수거업체</Button_2>
+                <Button_choice onClick={() => nav("/join/collector")}>수거업체</Button_choice>
             </div>
         </>
     )
